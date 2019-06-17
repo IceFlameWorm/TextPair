7ae2edced2f6084d52f6e9114e6a62d0e60dc3e5
aaron <aaron@empty.com>
2019-06-17 17:49:29 +0800

    测试跑通原型

--- web.py
+++ web.py
@@ -3,11 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def index():
     return render_template("index.html")
-    pass
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', threaded = True)
+    app.run(debug=True)
