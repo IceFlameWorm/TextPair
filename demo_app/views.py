d6a2c1905b482cb1bc62a84e3b0311d59bc64984
aaron <aaron@empty.com>
2019-07-04 16:03:16 +0800

    dev 更改首页的返回方式

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,5 +1,7 @@
 from .app import app
+from flask import render_template
 
 @app.route("/")
 def index():
-    return app.send_static_file("demo_index.html")
+    # return app.send_static_file("demo_index.html")
+    return render_template("demo_index.html")
