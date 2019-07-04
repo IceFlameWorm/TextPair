d4987b1646e77498b8a51b81fc3195ed61cbc0ba
aaron <aaron@empty.com>
2019-07-04 11:26:06 +0800

    dai更改index页面

--- demo_app/views.py
+++ demo_app/views.py
@@ -2,8 +2,4 @@ from .app import app
 
 @app.route("/")
 def index():
-<<<<<<< Updated upstream
     return app.send_static_file("index.html")
-=======
-    return app.send_static_file("index.html")
->>>>>>> Stashed changes
