0a4f57e8dc633e4ade98d0bd610148971be57e42
aaron <aaron@empty.com>
2019-07-04 17:59:35 +0800

    仍然试用静态方式返回index页面 dev

--- demo_app/views.py
+++ demo_app/views.py
@@ -3,5 +3,5 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    # return app.send_static_file("demo_index.html")
-    return render_template("demo_index.html")
+    return app.send_static_file("demo_index.html")
+    # return render_template("demo_index.html")
